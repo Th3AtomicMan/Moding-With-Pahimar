f5ec42be150f0339d39150ba354b1f3ee6a4bbf7
theatomicman <matthewrhuskey@gmail.com>
2014-06-28 10:56:18 -0500

    Update ep.4

---
+++ src/main/java/com/atomicMan/LetsModReboot/LetsModReboot.java
@@ -0,0 +1,32 @@
+package com.atomicMan.LetsModReboot;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid= "LetsModReboot", name = "Lets Mod Reboot", version = "1.7.2-1.1")
+
+public class LetsModReboot
+{
+    @Mod.Instance("LetsModReboot")
+    public static  LetsModReboot instance;
+
+    @Mod.EventHandler//Pre-Initialization
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler//Initialization
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler//Post-Initialization
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
+}
